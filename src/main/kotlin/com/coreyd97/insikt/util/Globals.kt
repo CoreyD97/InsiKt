cab797398e223383408c19a02eb0c718f0860ae3
corey <1339555+CoreyD97@users.noreply.github.com>
2025-11-08 14:57:57 +0000

    Bump version

--- src/main/kotlin/com/coreyd97/insikt/util/Globals.kt
+++ src/main/kotlin/com/coreyd97/insikt/util/Globals.kt
@@ -7,7 +7,7 @@ import java.util.regex.Pattern
 
 
 const val APP_NAME: String = "InsiKt"
-const val VERSION: String = "0.0.1"
+const val VERSION: String = "1.0.0"
 
 //Preferences Keys
 const val PREF_LOG_TABLE_SETTINGS: String = "tabledetailsjson"
